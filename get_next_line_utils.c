5b9132aa78e2b48bfa544c77c00a3e49c9ab3132
hxwvaa <hamdhann11@gmail.com>
2024-05-18 20:28:47 +0400

    get_next_line mandatory

---
+++ get_next_line_utils.c
@@ -0,0 +1,68 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s && s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)s + i);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = malloc(ft_strlen(s) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	char	*s;
+
+	if (!s1 && !s2)
+		return (NULL);
+	s = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (s == NULL)
+		return (NULL);
+	str = s;
+	if (s1)
+		while (*s1)
+			*str++ = *s1++;
+	if (s2)
+		while (*s2)
+			*str++ = *s2++;
+	*str = '\0';
+	return (s);
+}
